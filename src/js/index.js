16bb166dc21fe8d7a40e052f33e7309b239286b8
kikeestrada <kikeestrada@gmail.com>
2020-04-19 14:14:42 -0600

    ajustes sw

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 import {topNav} from './modules/topNav';
+import {swDetecter} from './modules/swDetecter';
 (()=>{
+
+	swDetecter();
 	topNav();
 	if (document.body.classList.contains('home')) {
 		// functions here
