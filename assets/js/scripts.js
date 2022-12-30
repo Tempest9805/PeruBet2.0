0b7a1c865192a9b7aa5e52bd05aa3ff1dc24cce9
Daniel Chaves <danielchavesvargas98@gmail.com>
2022-12-30 08:14:11 -0600

    Updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,45 @@
-!function(){return function e(t,n,o){function r(c,i){if(!n[c]){if(!t[c]){var s="function"==typeof require&&require;if(!i&&s)return s(c,!0);if(u)return u(c,!0);var a=new Error("Cannot find module '"+c+"'");throw a.code="MODULE_NOT_FOUND",a}var l=n[c]={exports:{}};t[c][0].call(l.exports,function(e){return r(t[c][1][e]||e)},l,l.exports,e,t,n,o)}return n[c].exports}for(var u="function"==typeof require&&require,c=0;c<o.length;c++)r(o[c]);return r}}()({1:[function(e,t,n){"use strict";(0,e("./modules/topNav").middleNav)(),document.body.classList.contains("home")||document.body.classList.contains("page2")||document.body.classList.contains("page3")||document.body.classList.contains("page4")},{"./modules/topNav":2}],2:[function(e,t,n){"use strict";Object.defineProperty(n,"__esModule",{value:!0});n.middleNav=function(){document.querySelector(".hamburger").addEventListener("click",function(e){e.preventDefault(),[].map.call(document.querySelectorAll(".hamburger"),function(e){e.classList.toggle("is-active")}),[].map.call(document.querySelectorAll(".middlenav__menu"),function(e){e.classList.toggle("show-middleNav")})})}},{}]},{},[1]);
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+'use strict';
+
+var _topNav = require('./modules/topNav');
+
+// import {swDetecter} from './modules/swDetSecter';
+(function () {
+
+	// swDetecter();
+	(0, _topNav.middleNav)();
+	if (document.body.classList.contains('home')) {
+		// functions here
+	} else if (document.body.classList.contains('page2')) {
+		// functions here
+	} else if (document.body.classList.contains('page3')) {
+		// functions here
+	} else if (document.body.classList.contains('page4')) {
+		// functions here
+	}
+})();
+
+},{"./modules/topNav":2}],2:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+var middleNav = exports.middleNav = function middleNav() {
+	var myFunction = function myFunction() {
+		document.querySelector('.hamburger').addEventListener('click', function (e) {
+			e.preventDefault();
+			[].map.call(document.querySelectorAll('.hamburger'), function (el) {
+				el.classList.toggle('is-active');
+			});
+			[].map.call(document.querySelectorAll('.middlenav__menu'), function (el) {
+				el.classList.toggle('show-middleNav');
+			});
+		});
+	};
+	myFunction();
+};
+
+},{}]},{},[1]);
+
 //# sourceMappingURL=scripts.js.map
