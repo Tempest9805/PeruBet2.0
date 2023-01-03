8376889fe331a53e79706520bf2ad25c66a1f023
Daniel Chaves <danielchavesvargas98@gmail.com>
2023-01-03 09:56:30 -0600

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,17 @@
 import {middleNav} from './modules/topNav';
+import {tnsCarousell,tnsCarousell2, tnsSingle, tnsVertical, tnsSyncing} from "./modules/tns-slider";
+
 // import {swDetecter} from './modules/swDetSecter';
 (()=>{
 
 	// swDetecter();
 	middleNav();
+	tnsCarousell();
+	tnsCarousell2();
+	// tnsSingle();
+	// tnsVertical();
+	// tnsSyncing();
+	// accordion();
 	if (document.body.classList.contains('home')) {
 		// functions here
 	}else if (document.body.classList.contains('page2')) {
